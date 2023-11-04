04dcec1218fe15279076aa997ef57f3c22960bfe
jidakarn <beemjidakarn@gmail.com>
2023-11-04 18:57:00 +0700

    create page landing

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,13 @@ import "./App.css";
 //components
 import Navbar from "./components/Navbar";
 import HeroBanner from "./components/HeroBanner";
+import About from "./components/About";
+import InfoOne from "./components/InfoOne";
+import Infotwo from "./components/Infotwo";
+import CTA from "./components/CTA";
+import Portfolio from "./components/Portfolio";
+import GetInTouch from "./components/GetInTouch";
+import Footer from "./components/Footer";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -14,6 +21,13 @@ function App() {
     <>
     <Navbar></Navbar>
     <HeroBanner></HeroBanner>
+    <About></About>
+    <InfoOne></InfoOne>
+    <Infotwo></Infotwo>
+    <CTA />
+    <Portfolio />
+    <GetInTouch />
+    <Footer />
     </>
   );
 }
--- src/components/HeroBanner.jsx
+++ src/components/HeroBanner.jsx
@@ -2,11 +2,10 @@ import React from "react";
 
 function HeroBanner() {
   return (
-    <div className="bg-blue-100 text-black py-16">
+    <div className="bg-gray-900 text-white py-16">
       <div className="container mx-auto text-center">
         <div className="text-5xl font-extrabold">Welcom to My FAN</div>
         <p className="text-lg mt-4">discover amazing thing with me</p>
-        <div className="btn-wrapper mt-8">
           <a
             href="javascript:;"
             target="_self"
@@ -14,7 +13,6 @@ function HeroBanner() {
           >
             Explorer more
           </a>
-        </div>
       </div>
     </div>
   );
